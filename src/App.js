5c23ff1bd60fd13ad187008ba2d3f0258c957a89
az <az@azs-MacBook-Pro.local>
2021-04-14 12:17:22 +0600

    alarm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import 'date-fns';
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import useSound from 'use-sound'
+import music from './music/music.mp3'
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+} from '@material-ui/pickers';
 
-function App() {
+export default function MaterialUIPickers() {
+  // The first commit of Material-UI
+  const [selectedDate, setSelectedDate] = React.useState(new Date(''));
+  const [clock, setClock] = React.useState(new Date())
+  const [playmusic] = useSound(music)
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+
+    let hours = new Date().getHours()
+    let minutes = new Date().getMinutes()
+    let seconds = new Date().getSeconds()
+
+    let time = ((hours * 3600) + (minutes*60) + seconds)*1000
+  
+    let hours1 = date.getHours()
+    let minutes1 = date.getMinutes()
+
+    let time1 = ((hours1 * 3600) + (minutes1*60))*1000
+
+    let timeOut = time1-time
+    console.log(timeOut)
+
+    setTimeout(()=>{
+      alert('TIME')
+      playmusic()
+    },timeOut)
+//если поставим 5000 вместо timeOut он выведет алерт через 5 сек
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justify="space-around">
+        <KeyboardTimePicker
+          margin="normal"
+          id="time-picker"
+          label="Time picker"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change time',
+          }}
+        />
+        <button
+        onClick='stopFunc()'
+        >TURN OFF</button>
+      </Grid>
+    </MuiPickersUtilsProvider>
   );
 }
-
-export default App;
